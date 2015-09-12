1bc688e4d82ff83293b37b6d1629a8df5cec4932
JasonPan <jason8399@gmail.com>
2015-09-13 00:53:54 +0800

    add DB connection, add getPostsId, add LoadCongfiguration, add example_server.conf, fix posts.tmpl

--- server.go
+++ server.go
@@ -1,23 +1,91 @@
 package main
 
+import "database/sql"
 import "github.com/gin-gonic/gin"
+import _ "github.com/go-sql-driver/mysql"
+import "gopkg.in/gcfg.v1"
+import "gopkg.in/gorp.v1"
+import "log"
 import "net/http"
 
+type PYQ struct {
+	Id		int64
+	Author	string
+	Title	string
+	Content	string
+	Time	string
+}
+
+type Config struct {
+	MysqlUsername	string
+	MysqlPassword	string
+	MysqlLocation	string
+	MysqlDbname		string
+}
+
+type configFile struct {
+	Server Config
+}
+
 func main() {
-    r := gin.Default()
-    r.LoadHTMLGlob("tmpl/*")
-    r.Static("/assets", "assets")
-    r.GET("/", func(c *gin.Context) {
+    route := gin.Default()
+    route.LoadHTMLGlob("tmpl/*")
+    route.Static("/assets", "assets")
+    route.GET("/", func(c *gin.Context) {
         c.HTML(http.StatusOK, "index.tmpl", gin.H {
             "title": "傳說中的考古題系統",
         })
     })
-    r.GET("/posts/:id", func(c *gin.Context) {
-        // id := c.Param("id")
-        c.String(http.StatusOK, "posts.tmpl", gin.H {
-            "title": "傳說中的考古題系統",
-            "content": "",
-        })
-    })
-    r.Run(":8000")
+    route.GET("/posts/:id", getPostsId)
+    route.Run(":8000")
 }
+
+var config = LoadConfiguration()
+
+func LoadConfiguration() Config {
+	var err error
+	var cfg configFile
+
+	err = gcfg.ReadFileInto(&cfg, "./server.conf")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return cfg.Server
+}
+
+var dbmap = initDb()
+
+func initDb() *gorp.DbMap {
+	db, err := sql.Open("mysql", config.MysqlUsername + ":" + config.MysqlPassword +
+						"@" + config.MysqlLocation + "/" + config.MysqlDbname)
+	checkErr(err ,"sql.Open failed")
+	dbmap := &gorp.DbMap{Db: db, Dialect: gorp.MySQLDialect{"InnoDB", "UTF8"}}
+	dbmap.AddTableWithName(PYQ{}, "PYQ").SetKeys(true, "Id")
+	err = dbmap.CreateTablesIfNotExists()
+	checkErr(err, "Create table failed")
+	return dbmap
+}
+
+func checkErr(err error, msg string) {
+	if err != nil {
+		log.Fatalln(msg, err)
+	}
+}
+
+func getPostsId(c *gin.Context) {
+	id := c.Param("id")
+	var content string
+	err := dbmap.SelectOne(&content, "SELECT content FROM PYQ WHERE id = ?", id)
+	if err == nil {
+		c.HTML(http.StatusOK, "posts.tmpl", gin.H {
+			"title": "傳說中的考古題系統",
+			"content": content,
+		})
+	}else{
+		c.String(http.StatusNotFound, "安安沒找到喔")
+	}
+}
+
+
