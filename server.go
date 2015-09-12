cea16af1bf98c9377aa21ce9e16b5825282df24c
knowlet <knowlet3389@gmail.com>
2015-09-13 05:03:18 +0800

    Add index tmpl post api

--- server.go
+++ server.go
@@ -11,29 +11,29 @@ import "strconv"
 import "fmt"
 
 type PYQ struct {
-	Id		int64
-	Author	string
-	Title	string
-	Content	string
-	Time	string
+    Id      int64
+    Author  string
+    Title   string
+    Content string
+    Time    string
 }
 
 type PostInfo struct {
-	Id		int64
-	Author	string
-	Title	string
-	Time	string
+    Id      int64
+    Author  string
+    Title   string
+    Time    string
 }
 
 type Config struct {
-	MysqlUsername	string
-	MysqlPassword	string
-	MysqlLocation	string
-	MysqlDbname		string
+    MysqlUsername   string
+    MysqlPassword   string
+    MysqlLocation   string
+    MysqlDbname     string
 }
 
 type configFile struct {
-	Server Config
+    Server Config
 }
 
 func main() {
@@ -46,68 +46,69 @@ func main() {
         })
     })
     route.GET("/posts/:id", getPostsId)
-	route.POST("/api/posts/", postApiPostsPage)
+    route.POST("/api/posts/", postApiPostsPage)
     route.Run(":8000")
 }
 
 var config = LoadConfiguration()
 
 func LoadConfiguration() Config {
-	var err error
-	var cfg configFile
+    var err error
+    var cfg configFile
 
-	err = gcfg.ReadFileInto(&cfg, "./server.conf")
+    err = gcfg.ReadFileInto(&cfg, "./server.conf")
 
-	if err != nil {
-		panic(err.Error())
-	}
+    if err != nil {
+        panic(err.Error())
+    }
 
-	return cfg.Server
+    return cfg.Server
 }
 
 var dbmap = initDb()
 
 func initDb() *gorp.DbMap {
-	db, err := sql.Open("mysql", config.MysqlUsername + ":" + config.MysqlPassword +
-						"@" + config.MysqlLocation + "/" + config.MysqlDbname)
-	checkErr(err ,"sql.Open failed")
-	dbmap := &gorp.DbMap{Db: db, Dialect: gorp.MySQLDialect{"InnoDB", "UTF8"}}
-	dbmap.AddTableWithName(PYQ{}, "PYQ").SetKeys(true, "Id")
-	err = dbmap.CreateTablesIfNotExists()
-	checkErr(err, "Create table failed")
-	return dbmap
+    db, err := sql.Open("mysql", config.MysqlUsername + ":" + config.MysqlPassword +
+                        "@" + config.MysqlLocation + "/" + config.MysqlDbname)
+    checkErr(err ,"sql.Open failed")
+    dbmap := &gorp.DbMap{Db: db, Dialect: gorp.MySQLDialect{"InnoDB", "UTF8"}}
+    dbmap.AddTableWithName(PYQ{}, "PYQ").SetKeys(true, "Id")
+    err = dbmap.CreateTablesIfNotExists()
+    checkErr(err, "Create table failed")
+    return dbmap
 }
 
 func checkErr(err error, msg string) {
-	if err != nil {
-		panic(err.Error())
-		log.Fatalln(msg, err)
-	}
+    if err != nil {
+        log.Fatalln(msg, err)
+    }
 }
 
 func getPostsId(c *gin.Context) {
-	id := c.Param("id")
-	var content string
-	err := dbmap.SelectOne(&content, "SELECT content FROM PYQ WHERE id = ?", id)
-	if err == nil {
-		c.HTML(http.StatusOK, "posts.tmpl", gin.H {
-			"title": "傳說中的考古題系統",
-			"content": content,
-		})
-	}else{
-		c.String(http.StatusNotFound, "安安沒找到喔")
-	}
+    id := c.Param("id")
+    var content string
+    err := dbmap.SelectOne(&content, "SELECT content FROM PYQ WHERE id = ?", id)
+    if err == nil {
+        c.HTML(http.StatusOK, "posts.tmpl", gin.H {
+            "title": "傳說中的考古題系統",
+            "content": content,
+        })
+    }
+    else {
+        c.String(http.StatusNotFound, "安安沒找到喔")
+    }
 }
 
 func postApiPostsPage(c *gin.Context) {
-	page, _ := strconv.ParseInt(c.PostForm("p"), 0, 64)
-	fmt.Printf("%d\n",page)
-	var list []PostInfo
-	_, err := dbmap.Select(&list, "SELECT id, author, title, time FROM PYQ LIMIT ?,10", (page - 1) * 10)
-	if err == nil {
-		c.JSON(http.StatusOK, list)
-	}else{
-		checkErr(err, "postApi Error")
-		c.JSON(http.StatusNotFound, gin.H{"error": "not found"})
-	}
+    page, _ := strconv.ParseInt(c.PostForm("p"), 0, 64)
+    fmt.Printf("%d\n",page)
+    var list []PostInfo
+    _, err := dbmap.Select(&list, "SELECT id, author, title, time FROM PYQ LIMIT ?,10", (page - 1) * 10)
+    if err == nil {
+        c.JSON(http.StatusOK, list)
+    }
+    else {
+        checkErr(err, "postApi Error")
+        c.JSON(http.StatusNotFound, gin.H{"error": "not found"})
+    }
 }
