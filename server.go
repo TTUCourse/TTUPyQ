3e485441cfa3037bf5a07ca400d56d04f171ec22
knowlet <knowlet3389@gmail.com>
2015-09-12 22:31:01 +0800

    Add index show json

--- server.go
+++ server.go
@@ -13,17 +13,11 @@ func main() {
         })
     })
     r.GET("/posts/:id", func(c *gin.Context) {
-        id := c.Param("id")
+        // id := c.Param("id")
         c.String(http.StatusOK, "posts.tmpl", gin.H {
             "title": "傳說中的考古題系統",
             "content": "",
         })
-    })
-    r.GET("/user/:name/*action", func(c *gin.Context) {
-        name := c.Param("name")
-        action := c.Param("action")
-        message := name + " is " + action
-        c.String(http.StatusOK, message)
     })
     r.Run(":8000")
 }
